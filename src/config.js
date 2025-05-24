e71b81395eabbb05642764dbc9aec7e3d1a898df
JocanaPerez <jop.ocana@gmail.com>
2025-05-24 15:18:46 -0500

    Update config.js
    exito

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 import { config } from "dotenv";
 config();
 
+export const PORT = process.env.PORT || 3000
 export const DB_HOST = process.env.DB_HOST || process.env.MYSQLHOST || 'localhost';
 export const DB_USER = process.env.DB_USER || process.env.MYSQLUSER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || process.env.MYSQLPASSWORD || 'root1234';
